addce1b30ae85d1848dea1bb718d5c16c4bad0db
Aravindkaspe <aravindkaspe1991@gmail.com>
2024-05-30 09:44:48 -0500

    Submitting updated Json files

--- Source/backend/index.js
+++ Source/backend/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import { PORT, MongoURL } from "./config.js";
+import contactRouter from "./routes/contactRoute.js";
+
 
 const app = express();
+
+app.use('/contact', contactRouter);
 
 app.get("/", (req, res) => {
     return res.status(200).send("<h1>Hello, World!</h1>");
---
+++ Source/backend/routes/contactRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const contactRouter = express.Router();
+
+contactRouter.get("/", async (req, res) => {
+    try {
+        
+
+    } catch (error) {
+        
+    }
+})
+
+
+export default contactRouter;
